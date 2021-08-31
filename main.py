72c7bd6c98b422b498af19d470915858aa77eba0
vedsimp <bettsygarces12@gmail.com>
2021-08-31 14:27:32 -0500

    ensayando

---
+++ main.py
@@ -0,0 +1,10 @@
+# AUTOR: Bettsy Liliana Garces Buritica
+# FECHA: 31 de Agosto de 2021
+# QUE HACE: Este programa saluda la usuario
+
+def saludar():
+    print("¡Hola mundo Bettsy!")
+def main():
+    saludar()
+    
+main()
